494aa0ddd6a79c7678a23a65c59b16e48b7d6963
delvinlow <delvinlowzy@gmail.com>
2016-03-05 14:58:34 +0800

    Final skeleton code using fragment

--- app/src/main/java/sg/edu/nus/oztrafficcamera/MainActivity.java
+++ app/src/main/java/sg/edu/nus/oztrafficcamera/MainActivity.java
@@ -1,7 +1,11 @@
 package sg.edu.nus.oztrafficcamera;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void all_sensors(View view){
+
+    }
+
+    public void go_to_camera(View view){
+
+    }
+
+    public void go_to_microphone(View view){
+
+    }
+
+    public void go_to_accelerometer(View view){
+
+    }
+
 }
